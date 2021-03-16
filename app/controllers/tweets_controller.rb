2693509546d419ae236e4c248b7824ca6ecbe5c7
shota0412 <sh.v.w0412@gmail.com>
2021-03-16 12:12:16 +0900

    新規投稿ページ

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,22 @@
 class TweetsController < ApplicationController
   def index
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    @tweet.save
+    redirect_to tweet_path(@tweet)
+  end
+
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:text, :image).merge(user_id: current_user.id)
+  end
+
 end
