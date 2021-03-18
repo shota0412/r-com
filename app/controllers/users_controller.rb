46c134248c7503f480d6a6372f2656b1514ef534
shota0412 <sh.v.w0412@gmail.com>
2021-03-18 23:44:41 +0900

    userコントローラー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
 
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+  
   private
   def user_params
     params.require(:user).permit(:username, :email, :profile, :profile_image)
