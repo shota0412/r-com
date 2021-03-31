fa2ac62231c6cd708b240e3c6d4912ca0b88e65e
shota0412 <sh.v.w0412@gmail.com>
2021-03-31 17:51:36 +0900

    エラー解消

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show]
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user)
   end
 
   def new
